f5cb6ef51e5582a86754fad84359150565cc472f
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-12-08 13:02:17 +0800

    Add feature to add new books

--- library.js
+++ library.js
@@ -1,8 +1,34 @@
-const myLibrary = ["Perfect Veil",
-                     "The Sunken Mirror",
-                     "Wild and Wicked",
-                     "Prince Charmer",
-                     "Rich Young Boy"];
+const myLibrary = [
+                    {
+                        title: "The Great Gatsby",
+                        author: "F. Scott Fitzgerald",
+                        pages: "213",
+                        status: "Not read yet"
+                    },
+                    {
+                        title: "1984",
+                        author: "George Orwell",
+                        pages: "322",
+                        status: "Read"
+                    },
+                    {
+                        title: "Don Quixote",
+                        author: "Miguel de Cervantes",
+                        pages: "180",
+                        status: "Not read yet"
+                    },
+                    {
+                        title: "To Kill A Mockingbird",
+                        author: "Harper Lee",
+                        pages: "169",
+                        status: "Read"
+                    },
+                    {
+                        title: "Pride and Prejudice",
+                        author: "Jane Austen",
+                        pages: "158",
+                        status: "Not read yet"
+                    }];
 
 function Book(title, author, pages, isRead) {
     //constructor
@@ -10,22 +36,64 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.status = isRead != "on" ? "Read" : "Not read yet";
 }
 
 function addBookToLibrary(newBook){
-
     myLibrary.push(newBook);
     displayBooks();
 }
 
+//Update library
 function displayBooks(){
     const container = document.getElementById("container");
+    container.textContent = "";
     myLibrary.forEach(function(book){
         const div = document.createElement("div");
-        div.textContent = book;
+        div.textContent = book.title + "" +
+                            book.author +
+                            book.pages +
+                            book.status;
         div.className = "bookDiv";
         container.appendChild(div);
     })
 }
+
+const dialog = document.querySelector("dialog");
+const newBook = document.getElementById("newBook");
+const closeBtn = document.getElementById("closeBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+//Open add book form
+newBook.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+//Close add book form
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+})
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const isRead = document.getElementById("isRead");
+
+//Submit new book
+submitBtn.addEventListener("click", (event) => {
+    //Take input content and make properties of new object
+    event.preventDefault();
+    const bookTitle = title.value;
+    const bookAuthor = author.value;
+    const bookPages = pages.value;
+    const bookStatus = isRead.checked;
+    const book = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+    addBookToLibrary(book);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    isRead.checked = false;
+    dialog.close();
+})
 
 displayBooks();
